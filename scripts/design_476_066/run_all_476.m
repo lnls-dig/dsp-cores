6d6727f06d59f84bd361815585d858d9edc63b8d
Lucas Russo <lerwys@gmail.com>
2013-06-25 13:19:10 -0300

    scripts/*: add missing scripts and cosmetic changes

--- scripts/design_476_066/run_all_476.m
+++ scripts/design_476_066/run_all_476.m
@@ -4,7 +4,7 @@ general_const_476;
 %script_singlebunch_no_graphs_2_476;
 ddc_design_no_graphs_476;
 %Hbpf_adc = bpf_adc_design_no_graphs_476(Fs_adc, Fs_adc_image);
-Hbpf_adc = bpf_adc_design_no_graphs2_476(Fs_adc, Fs_adc_image);
+Hbpf_adc = bpf_adc_design_no_graphs3_476(Fs_adc, Fs_adc_image);
 dds_freq = calc_dds_freq(Fs_adc, Fs_adc_image);
 dds_freq_bin = dec2bin(dds_freq);
 vrms_att = dbm2vrms(-10)-[dbm2vrms(-10.001)
