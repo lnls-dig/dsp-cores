71e733cf66e9d6805c9ad2a93aedb780d6f36c62
Lucas Russo <lerwys@gmail.com>
2013-07-12 11:32:50 -0300

    hdl/*/wb_position_calc/*: integrate dsp chain with switching module
    Former-commit-id: 1299e608eeb8991a0dddde7895fc5fd6bcd403ad

---
+++ hdl/modules/wb_position_calc/position_calc_core_pkg.vhd
@@ -0,0 +1,27 @@
+------------------------------------------------------------------------------
+-- Title      : Wishbone FMC516 ADC Interface
+------------------------------------------------------------------------------
+-- Author     : Lucas Maziero Russo
+-- Company    : CNPEM LNLS-DIG
+-- Created    : 2013-12-07
+-- Platform   : FPGA-generic
+-------------------------------------------------------------------------------
+-- Description: General definitions package for position calc core
+-------------------------------------------------------------------------------
+-- Copyright (c) 2012 CNPEM
+-- Licensed under GNU Lesser General Public License (LGPL) v3.0
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author          Description
+-- 2013-12-07  1.0      lucas.russo        Created
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library work;
+use work.wishbone_pkg.all;
+
+package position_calc_core_pkg is
+
+end position_calc_core_pkg;
