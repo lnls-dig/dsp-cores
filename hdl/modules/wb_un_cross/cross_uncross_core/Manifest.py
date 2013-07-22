eeb1e4020b0afd2df76b6bf6a2d1d34dbfb7635d
Lucas Russo <lerwys@gmail.com>
2013-07-22 17:39:49 -0300

    hdl/platform/virtex6/*: move 16x10 multiplier to platform folder
    Former-commit-id: 7609940e9fd15193f0380974a1bf23f22eedb528

--- hdl/modules/wb_un_cross/cross_uncross_core/Manifest.py
+++ hdl/modules/wb_un_cross/cross_uncross_core/Manifest.py
@@ -4,7 +4,5 @@ files = [ "un_cross_top.vhd",
           "inv_ch.vhd",
           "delay_inv_ch.vhd",
           "inv_chs_top.vhd",
-          "multiplier_16x10_DSP.vhd",
-          "multiplier_16x10_DSP.ngc",
           "dyn_mult_2chs.vhd"
          ];
--- hdl/platform/virtex6/Manifest.py
+++ hdl/platform/virtex6/Manifest.py
@@ -1,2 +1,4 @@
 files = ["multiplier_u16x16_DSP.ngc",
-	"multiplier_u16x16_DSP.vhd"]
+	"multiplier_u16x16_DSP.vhd",
+	"multiplier_16x10_DSP.ngc",
+	"multiplier_16x10_DSP.vhd"]
