094f2410b6eb304fd9c85a83b1a2f88158b33376
Lucas Russo <lerwys@gmail.com>
2013-08-29 14:42:51 -0300

    hdl/platform/artix7/*: add artix7 cores

--- hdl/Manifest.py
+++ hdl/Manifest.py
@@ -5,8 +5,8 @@ def __dirs():
 		dirs.extend(["platform/virtex6"]);
 	#elif (target == "xilinx" and syn_device[0:4].upper()=="XC7K"):
 	#	dirs.extend(["platform/kintex7/ip_cores"]);
-	#elif (target == "xilinx" and syn_device[0:4].upper()=="XC7A"):
-	#	dirs.extend(["platform/artix7/ip_cores"]);
+	elif (target == "xilinx" and syn_device[0:4].upper()=="XC7A"):
+		dirs.extend(["platform/artix7"]);
 	#else: #add paltform here and generate the corresponding ip cores
 	return dirs
 
