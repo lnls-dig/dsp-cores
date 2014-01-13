9fbd1ea2dd7b5868fb6c019703205f97ffebfeb5
Lucas Russo <lerwys@gmail.com>
2014-01-13 18:53:00 -0200

    hdl/modules/*: change location of SDB structures again.

--- hdl/modules/dsp_cores_pkg.vhd
+++ hdl/modules/dsp_cores_pkg.vhd
@@ -855,40 +855,4 @@ package dsp_cores_pkg is
   );
   end component;
 
-  --------------------------------------------------------------------
-  -- SDB Devices Structures
-  --------------------------------------------------------------------
-
-  constant c_xwb_bpm_swap_sdb : t_sdb_device := (
-    abi_class     => x"0000",                 -- undocumented device
-    abi_ver_major => x"01",
-    abi_ver_minor => x"00",
-    wbd_endian    => c_sdb_endian_big,
-    wbd_width     => x"4",                     -- 8/16/32-bit port granularity (0100)
-    sdb_component => (
-    addr_first    => x"0000000000000000",
-    addr_last     => x"00000000000000FF",
-    product => (
-    vendor_id     => x"1000000000001215",     -- LNLS
-    device_id     => x"12897592",
-    version       => x"00000001",
-    date          => x"20130703",
-    name          => "LNLS_BPM_SWAP      ")));
-
-  constant c_xwb_position_calc_core_sdb : t_sdb_device := (
-    abi_class     => x"0000",                 -- undocumented device
-    abi_ver_major => x"01",
-    abi_ver_minor => x"00",
-    wbd_endian    => c_sdb_endian_big,
-    wbd_width     => x"4",                     -- 8/16/32-bit port granularity (0100)
-    sdb_component => (
-    addr_first    => x"0000000000000000",
-    addr_last     => x"0000000000000FFF",
-    product => (
-    vendor_id     => x"1000000000001215",     -- LNLS
-    device_id     => x"1bafbf1e",
-    version       => x"00000001",
-    date          => x"20130703",
-    name          => "LNLS_POSITION_CALC ")));
-
 end dsp_cores_pkg;
--- hdl/modules/wb_position_calc/position_calc_core_pkg.vhd
+++ hdl/modules/wb_position_calc/position_calc_core_pkg.vhd
@@ -95,4 +95,40 @@ package position_calc_core_pkg is
   );
   end component;
 
+  --------------------------------------------------------------------
+  -- SDB Devices Structures
+  --------------------------------------------------------------------
+
+  constant c_xwb_bpm_swap_sdb : t_sdb_device := (
+    abi_class     => x"0000",                 -- undocumented device
+    abi_ver_major => x"01",
+    abi_ver_minor => x"00",
+    wbd_endian    => c_sdb_endian_big,
+    wbd_width     => x"4",                     -- 8/16/32-bit port granularity (0100)
+    sdb_component => (
+    addr_first    => x"0000000000000000",
+    addr_last     => x"00000000000000FF",
+    product => (
+    vendor_id     => x"1000000000001215",     -- LNLS
+    device_id     => x"12897592",
+    version       => x"00000001",
+    date          => x"20130703",
+    name          => "LNLS_BPM_SWAP      ")));
+
+  constant c_xwb_pos_calc_core_regs_sdb : t_sdb_device := (
+    abi_class     => x"0000",                 -- undocumented device
+    abi_ver_major => x"01",
+    abi_ver_minor => x"00",
+    wbd_endian    => c_sdb_endian_big,
+    wbd_width     => x"4",                     -- 8/16/32-bit port granularity (0100)
+    sdb_component => (
+    addr_first    => x"0000000000000000",
+    addr_last     => x"00000000000000FF",
+    product => (
+    vendor_id     => x"1000000000001215",     -- LNLS
+    device_id     => x"1bafbf1e",
+    version       => x"00000001",
+    date          => x"20130703",
+    name          => "LNLS_POS_CALC_REGS ")));
+
 end position_calc_core_pkg;
