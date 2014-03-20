cc7938c4c8b81e7a58278865961a2e0d105a0a45
Lucas Russo <lerwys@gmail.com>
2014-03-20 17:50:55 -0300

    hdl/modules/sw_windowing: (3/5) add switching clock enable input
    Use switching clock enable in the clock generation logic

--- hdl/modules/sw_windowing/counter.vhd
+++ hdl/modules/sw_windowing/counter.vhd
@@ -39,6 +39,7 @@ entity counter is
     ce_i      : in  std_logic;          -- clock enable
     reset_n_i : in  std_logic;          -- reset
     switch_delay_i : in std_logic_vector(15 downto 0);
+    switch_en_i : in std_logic;
     switch_o  : out std_logic;
     index_o   : out std_logic_vector(g_bus_size-1 downto 0));  -- Memory address to current
                                                              -- window data
@@ -68,38 +69,44 @@ begin  -- architecture behavioural
         switch_state <= '0';
       else 
 	if ce_i = '1' then
+	  if switch_en_i = '0' then
+            count        := (others => '0');
+            going_up     := true;
+            switch_state <= '0';
+	  else
 
-          if going_up then
-            count := count + 1;
+            if going_up then
+              count := count + 1;
 
-            if count = last_address then
-              going_up := false;
-            end if;  --count = last_address
+              if count = last_address then
+                going_up := false;
+              end if;  --count = last_address
 
-            -- toggle switch clock. FIXME: switch_delay_slice
-            -- cannot be greater than last_address, otherwise
-            -- we will not switch properly
-            if count = last_address - unsigned(switch_delay_slice) then
-              switch_state <= not switch_state;
-            end if;
+              -- toggle switch clock. FIXME: switch_delay_slice
+              -- cannot be greater than last_address, otherwise
+              -- we will not switch properly
+              if count = last_address - unsigned(switch_delay_slice) then
+                switch_state <= not switch_state;
+              end if;
 
-          else
-            --counting down 
-            count := count - 1;
-            if count = to_unsigned(0, g_bus_size) then
-              going_up := true;
-            end if;  -- count = 0
+            else
+              --counting down 
+              count := count - 1;
+              if count = to_unsigned(0, g_bus_size) then
+                going_up := true;
+              end if;  -- count = 0
 
-            --switch N samples before reaches zero
-            --if count = g_switch_delay then
-            if count = unsigned(switch_delay_slice) then
-              switch_state <= not switch_state;
-            end if;  -- count = switch_state
-          end if;  -- going up
+              --switch N samples before reaches zero
+              --if count = g_switch_delay then
+              if count = unsigned(switch_delay_slice) then
+                switch_state <= not switch_state;
+              end if;  -- count = switch_state
+            end if;  -- going up
 
-          index_o  <= std_logic_vector(count);
-          switch_o <= switch_state;
+            index_o  <= std_logic_vector(count);
+            switch_o <= switch_state;
 
+	  end if; -- switch_en_i
         end if; -- ce
       end if;  -- reset
     end if;  -- rising edge
