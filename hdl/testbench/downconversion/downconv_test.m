bccbdd09ae3532d5141b69dcc741834f7d82fb63
Aylons Hazzud <gustavo@ayons.com.br>
2014-05-15 18:09:11 -0300

    Downconversion tests
    Former-commit-id: 1d8731b2d29fff5cbedca4ca9b965a0bcf77a37a

--- hdl/testbench/downconversion/downconv_test.m
+++ hdl/testbench/downconversion/downconv_test.m
@@ -3,8 +3,6 @@ freq = 120e3
 data = importdata('downconv_out.samples','\t',1);
 I = data.data(:,1)/(2^31);
 Q = data.data(:,2)/(2^31);
-mag = data.data(:,3)/(2^31);
-phase = data.data(:,4)/(2^31);
 
 %% Calculating from I, Q
 complex = I+1i*Q;   % Ensemble I and Q in a single MATLAB complex variable
@@ -15,10 +13,3 @@ mag_calc = mag_calc(1:floor(length(mag_calc)/60)*60);%/max(mag_calc);
 % Compute FFTs
 [MAF_calc, ff_mag_calc] = fourierseries(mag_calc, freq);
 plot(ff_mag_calc, 20*log10(MAF_calc),'.b'); hold on
-
-%% Straight from CORDIC
-mag = mag(50:end);
-mag_cordic = mag(1:floor(length(mag)/60)*60);%max(mag);
-
-[MAF_cord, ff_mag_cordic] = fourierseries(mag_cordic, freq);
-plot(ff_mag_cordic, 20*log10(MAF_cord),'.r'); hold on
--- hdl/testbench/downconversion/wave.do
+++ hdl/testbench/downconversion/wave.do
@@ -1,7 +1,5 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
 log //*
-log //uut_cic_i/*
-log //uut_cic_q/*
 
 
