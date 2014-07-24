126c0ff41c68f81a8592a48f2682aa1ea55f5b48
Aylons Hazzud <gustavo@ayons.com.br>
2014-07-24 14:43:22 -0300

    Generalized input conditioner

--- hdl/top/ml605/ddc_chain/input_conditioner.vhd
+++ hdl/top/ml605/ddc_chain/input_conditioner.vhd
@@ -6,7 +6,7 @@
 -- Author     : Gustavo BM Bruno
 -- Company    : 
 -- Created    : 2014-01-30
--- Last update: 2014-02-21
+-- Last update: 2014-02-25
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -34,14 +34,17 @@ use work.genram_pkg.all;
 entity input_conditioner is
   
   generic (
-    g_clk_freq : real := 120.0e6;       -- System clock frequency
-    g_sw_freq  : real := 100.0e3);      -- Desired switching frequency
+    g_clk_freq     : real    := 120.0e6;  -- System clock frequency
+    g_sw_freq      : real    := 100.0e3;  -- Desired switching frequency
+    g_input_width  : natural := 16;
+    g_output_width : natural := 24;
+    g_window_width : natural := 24);
   port (
     reset_n_i     : in  std_logic;      -- Reset data
     clk_i         : in  std_logic;      -- Main clock
-    adc_data_i    : in  std_logic_vector(15 downto 0);  -- Raw data from the ADC
+    adc_data_i    : in  std_logic_vector(g_input_width-1 downto 0);  -- Raw data from the ADC
     switch_o      : out std_logic;      -- Switch position output
-    data_output_o : out std_logic_vector(23 downto 0));  -- Windowed output data
+    data_output_o : out std_logic_vector(g_output_width-1 downto 0));  -- Windowed output data
 
 end entity input_conditioner;
 
@@ -50,8 +53,8 @@ architecture structural of input_conditioner is
   constant bus_size : natural := f_log2_size(mem_size);
 
   signal cur_address   : std_logic_vector(bus_size-1 downto 0) := (others => '0');  -- Current index for lookup table
-  signal window_factor : std_logic_vector(23 downto 0);  -- Current value of the window
-                                                         -- factor, signed int
+  signal window_factor : std_logic_vector(g_window_width downto 0);  -- Current value of the window
+                                        -- factor, signed int
   component generic_simple_dpram is
     generic (
       g_data_width               : natural;
@@ -72,16 +75,6 @@ architecture structural of input_conditioner is
       qb_o    : out std_logic_vector(g_data_width -1 downto 0));
   end component generic_simple_dpram;
 
-  component multiplier_s24_s24_s24 is
-    port (
-      clk  : in  std_logic;
-      a    : in  std_logic_vector(23 downto 0);
-      b    : in  std_logic_vector(23 downto 0);
-      sclr : in  std_logic;
-      ce   : in  std_logic;
-      p    : out std_logic_vector(23 downto 0));
-  end component multiplier_s24_s24_s24;
-
   component counter is
     generic (
       mem_size : natural;
@@ -98,7 +91,7 @@ begin
 
   cmp_lut : generic_simple_dpram
     generic map (
-      g_data_width               => 24,
+      g_data_width               => g_window_width,
       g_size                     => mem_size,
       g_with_byte_enable         => false,
       g_addr_conflict_resolution => "dont_care",
@@ -130,14 +123,18 @@ begin
       switch_o  => switch_o
       );
 
-  cpm_mult : multiplier_s24_s24_s24
-    port map (
-      clk            => clk_i,
-      a(23 downto 8) => adc_data_i,
-      a(7 downto 0)  => (others => '0'),
-      b              => window_factor,
-      sclr           => '0',
-      ce             => '1',
-      p              => data_output_o);
+  multiplication : process(clk_i)
+    constant c_result_width = g_input_width+g_window_width;
+    variable result : signed(c_result_width-1 downto 0);
+  begin
+    if rising_edge(clk_i) then
+      if reset_n_i = '0' then
+        data_output_o <= (other => '0');
+      else
+        result        := adc_data_i*window_factor;
+        data_output_o <= result(c_result_width-2 downto c_result_width - g_output_width - 1);
+      end if;  -- reset
+    end if;  -- clk
+  end process multiplication;
 
 end structural;
