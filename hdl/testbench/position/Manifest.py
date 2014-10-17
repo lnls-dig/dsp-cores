a3872bf134aa28bf40b772c9946112e29a8d9f08
Lucas Russo <lerwys@gmail.com>
2014-10-17 19:56:06 -0300

    hdl/testbench/position/Manifest.py: update to AFCv3 (artix-7) board

--- hdl/testbench/position/Manifest.py
+++ hdl/testbench/position/Manifest.py
@@ -1,7 +1,8 @@
 action = "simulation"
 target = "xilinx"
-syn_device = "xc6vlx240t"
+syn_device = "xc7a200t"
 
-modules = {"local" : ["../../"]}
+modules = {"local" : ["../../",
+                        "../../ip_cores/general-cores/"]}
 
 files = ["position_tb.vhd"]
