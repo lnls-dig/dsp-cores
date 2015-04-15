13738d1d42936dc0a16d00360d5a5a9c5b222c70
Aylons Hazzud <gustavo@aylons.com.br>
2015-04-15 08:20:42 -0300

    Moved UVX DDS LUT to inside the machine module.
    To allow different machines in the same code to be chosen during compile
    time, now LUT blocks must be located inside the 'machine' module. They
    will be added to the design by the hdlmake variable 'machine'.

--- hdl/modules/machine/uvx_130M/Manifest.py
+++ hdl/modules/machine/uvx_130M/Manifest.py
@@ -1,1 +1,9 @@
-files = [  "machine_pkg.vhd"]
+files = [  "machine_pkg.vhd",
+           "cos_lut_uvx_35_148/cos_lut_uvx_35_148.mif",
+           "cos_lut_uvx_35_148/cos_lut_uvx_35_148_ooc.xdc",
+           "cos_lut_uvx_35_148/cos_lut_uvx_35_148.xci",
+           "cos_lut_uvx_35_148/cos_lut_uvx_35_148.vhd",
+           "sin_lut_uvx_35_148/sin_lut_uvx_35_148.mif",
+           "sin_lut_uvx_35_148/sin_lut_uvx_35_148_ooc.xdc",
+           "sin_lut_uvx_35_148/sin_lut_uvx_35_148.xci",
+           "sin_lut_uvx_35_148/sin_lut_uvx_35_148.vhd"]
