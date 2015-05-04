a6ae7c74974c9c6de97f6fd185875223a92f5bc2
Aylons Hazzud <gustavo@aylons.com.br>
2015-05-04 16:20:44 -0300

    Altered machine module to throw error for unrecognized machine.

--- hdl/modules/machine/Manifest.py
+++ hdl/modules/machine/Manifest.py
@@ -1,1 +1,8 @@
-modules = { "local" : machine_pkg }
+# TODO: if hdlmake allowed for importing python packages in inner modules, this could existing directories directly
+
+if (machine_pkg == "uvx_130M"):
+    modules = { "local" : "uvx_130M"}
+elif (machine_pkg == "sirius_130M"):
+    modules = { "local" : "sirius_130M"}
+else:
+    quit("Invalid machine module! Check machine_pkg variable!")
