b0203d555081a059cdfa81d15c75021afaf8fe56
Vfinotti <finotao@gmail.com>
2015-07-01 17:34:00 -0300

    Fixed Manifest.py to run simulation.

--- hdl/testbench/daphine/Manifest.py
+++ hdl/testbench/daphine/Manifest.py
@@ -1,9 +1,13 @@
 action = "simulation"
 target = "xilinx"
-syn_device = "xc6vlx240t"
+vcom_opt = "-2008"
+
+syn_device = "xc7a200t"
 sim_tool = "modelsim"
 top_module = "daphine_tb"
 
-modules = {"local" : ["../../"]}
+modules = {"local" : ["../../",  "../../ip_cores/general-cores/"]}
 
 files = ["daphine_tb.vhd"]
+
+machine_pkg = "uvx_130M"
