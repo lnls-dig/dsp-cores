46c70a12949c566c9ce37f2bc263413cc16f1a28
Vfinotti <vfinotti@gmail.com>
2015-07-27 16:57:30 -0300

    Sink first implemention.

--- hdl/modules/wbstream/wb_stream_sink.vhd
+++ hdl/modules/wbstream/wb_stream_sink.vhd
@@ -1,45 +1,37 @@
 -------------------------------------------------------------------------------
--- Title      : Wishbone Packet Fabric buffered packet sink
--- Project    : WR Cores Collection
+-- Title      : Wishbone Stream Sink
+-- Project    : 
 -------------------------------------------------------------------------------
--- File       : xwb_fabric_sink.vhd
--- Author     : Tomasz Wlostowski
--- Company    : CERN BE-CO-HT
--- Created    : 2012-01-16
--- Last update: 2015-07-23
--- Platform   :
+-- File       : wb_stream_sink.vhd
+-- Author     : Vitor Finotti Ferreira  <finotti@finotti-Inspiron-7520>
+-- Company    : 
+-- Created    : 2015-07-27
+-- Last update: 2015-07-27
+-- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
--- Description: A simple WB packet streaming sink with builtin FIFO buffer.
--- Outputs a trivial interface (start-of-packet, end-of-packet, data-valid)
+-- Description: A simple WB packet streaming sink.
 -------------------------------------------------------------------------------
+-- Copyright (c) 2015     
+
+-- This program is free software: you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public License
+-- as published by the Free Software Foundation, either version 3 of
+-- the License, or (at your option) any later version.
 --
--- Copyright (c) 2011 CERN
+-- This program is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Lesser General Public License for more details.
 --
--- This source file is free software; you can redistribute it
--- and/or modify it under the terms of the GNU Lesser General
--- Public License as published by the Free Software Foundation;
--- either version 2.1 of the License, or (at your option) any
--- later version.
---
--- This source is distributed in the hope that it will be
--- useful, but WITHOUT ANY WARRANTY; without even the implied
--- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
--- PURPOSE.  See the GNU Lesser General Public License for more
--- details.
---
--- You should have received a copy of the GNU Lesser General
--- Public License along with this source; if not, download it
--- from http://www.gnu.org/licenses/lgpl-2.1.html
---
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this program. If not, see
+-- <http://www.gnu.org/licenses/>.
 -------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version  Author          Description
--- 2011-01-16  1.0      twlostow        Created
+-- 2015-07-27  1.0      vfinotti        Created
 -------------------------------------------------------------------------------
--- Modified by Lucas Russo <lucas.russo@lnls.br>
--- Modified by Gustavo Bruno <gustavo.bruno@lnls.br>
--- Modified by Vitor Finotti Ferreira <vitor.ferreira@lnls.br>
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -51,23 +43,23 @@ use work.wb_stream_pkg.all;
 
 entity wb_stream_sink is
   generic(
-    g_data_width   : natural := 32;
-    g_addr_width   : natural := 4;
-    g_tgd_width    : natural := 4;
-    g_buffer_depth : natural := 4
+    g_dat_width : natural := 32;
+    g_adr_width : natural := 4;
+    g_tgd_width : natural := 4;
     );
 
   port (
     clk_i : in std_logic;
     rst_i : in std_logic;
+    ce_i  : in std_logic;
 
     -- Wishbone Fabric Interface I/O
     snk_i : in  t_wbs_sink_in;
     snk_o : out t_wbs_sink_out;
 
     -- Decoded & buffered fabric
-    addr_o   : out std_logic_vector(g_addr_width-1 downto 0);
-    data_o   : out std_logic_vector(g_data_width-1 downto 0);
+    addr_o   : out std_logic_vector(g_adr_width-1 downto 0);
+    data_o   : out std_logic_vector(g_dat_width-1 downto 0);
     tgd_o    : out std_logic_vector(g_tgd_width-1 downto 0);
     dvalid_o : out std_logic;
     busy_i   : in  std_logic
@@ -82,43 +74,72 @@ architecture behavior of wb_stream_sink is
 
   -- Creating other input/output registers
   signal r_snk_ack_o : out std_logic := '0';
+  signal r_snk_stl_o : out std_logic := '0';
 
-  signal r_addr_o : out std_logic_vector(g_addr_width-1 downto 0);
-  signal r_data_o : out std_logic_vector(g_data_width-1 downto 0);
-  signal r_tgd_o  : out std_logic_vector(g_tgd_width-1 downto 0);
+  signal r_addr_o   : out std_logic_vector(g_addr_width-1 downto 0);
+  signal r_data_o   : out std_logic_vector(g_data_width-1 downto 0);
+  signal r_tgd_o    : out std_logic_vector(g_tgd_width-1 downto 0);
+  signal r_dvalid_o : out std_logic := '0';
 
+  signal r_mid_addr : out std_logic_vector(g_addr_width-1 downto 0);
+  signal r_mid_data : out std_logic_vector(g_data_width-1 downto 0);
+  signal r_mid_tgd  : out std_logic_vector(g_tgd_width-1 downto 0);
 
 begin
 
   -- Combinatinal logic
-  ack         <= (snk_i.cyc and snk_i.stb and not (busy_i));
-  snk_o.stall <= busy_i;
-  dvalid_o    <= r_snk_ack_o;
+  en_rd <= (snk_i.cyc and snk_i.stb and not (busy_i));
 
   clock_process : process (clk_i, rst_i) is
   begin  -- process clock_process
 
     if rising_edge(clk_i) then
       if rst_i then
-        -- Reset registers
+        -- Reset registers;
+
         r_snk_ack_o <= '0';
+        r_snk_stl_o <= '0';
 
-        r_addr_o <= (others => 'X');
-        r_data_o <= (others => 'X');
-        r_tgd_o  <= (others => 'X');
-        
-      elsif (ack) then
-        r_snk_ack_o < = '1';
+        r_addr_o   <= (others => 'X');
+        r_data_o   <= (others => 'X');
+        r_tgd_o    <= (others => 'X');
+        r_dvalid_o <= '0';
 
-        r_addr_o <= snk_i.addr;
-        r_data_o <= snk_i.data;
-        r_tgd_o  <= snk_i.tgd;
+        r_mid_addr <= (others => 'X');
+        r_mid_data <= (others => 'X');
+        r_mid_tgd  <= (others => 'X');
+
+      -- Writing outputs  
+      elsif (ce_i) then
+        if (rd_en) then
+          if (r_snk_stl_o) then
+            r_addr_o <= r_mid_addr;
+            r_data_o <= r_mid_data;
+            r_tgd_o  <= r_mid_tgd;
+          else
+            r_addr_o <= snk_i.addr;
+            r_data_o <= snk_i.data;
+            r_tgd_o  <= snk_i.tgd;
+          end if;
+        end if;
+
+        -- Storing temporary inputs
+        if (not (stall) and busy_i) then
+          r_mid_addr_o <= snk_i.adr;
+          r_mid_data_o <= snk_i.dat;
+          r_mid_tgd_o  <= snk_i.tgd;
+        end if;
+
+        -- assert valid/invalid data
+        if not(busy_i) then
+          r_dvalid_o <= en_rd;
+        end if;
       end if;
     end if;
-    
-  end process clock_process;
 
-  -- Connecting outputs
-  snk_o.ack <= r_snk_ack_o;
+    -- Connecting outputs
+    snk_o.ack <= en_rd;
+    snk_o.stl <= busy_i;
+  end process clock_process;
 
 end behavior;
--- hdl/testbench/wbstream/wb_stream_sink_tb.vhd
+++ hdl/testbench/wbstream/wb_stream_sink_tb.vhd
@@ -6,7 +6,7 @@
 -- Author     : Vitor Finotti Ferreira  <finotti@finotti-Inspiron-7520>
 -- Company    : 
 -- Created    : 2015-07-22
--- Last update: 2015-07-22
+-- Last update: 2015-07-23
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -30,96 +30,137 @@
 -------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version  Author  Description
--- 2015-07-22  1.0      finotti	Created
+-- 2015-07-22  1.0      finotti Created
 -------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
 
+library work;
 use work.wb_stream_pkg.all;
+use work.test_pkg.all;
 
 -------------------------------------------------------------------------------
 
 entity wb_stream_sink_tb is
 
-architecture test of wb_stream_sink_tb is
+  architecture test of wb_stream_sink_tb is
 
-  --constant c_input_freq : real := 120.0e6;
-  --constant clock_period : time := 1.0 sec / (2.0 * c_input_freq);
+      -- Test_pkg constants
+      constant c_CLK_FREQ : real := 100.0e6;        -- input clock frequency
+        constant c_CYCLES_TO_RESET : natural := 4;  -- number of clock cycles before reset
+        constant c_CYCLES_TO_CE    : natural := 10;  -- number of clock cycles before reset
 
-  --constant c_ce_rate : natural := 4;
+        constant c_INPUT_WIDTH    : positive   := 32;
+        constant c_OUTPUT_WIDTH   : positive   := 32;
+        constant c_INTERNAL_WIDTH : positive   := 38;  -- output_width + log2(c_ITER) +
+                                                       -- 2
+        -- Test_pkg signals
+        signal clk                : std_ulogic := '0';  -- clock signal
+        signal rst                : std_ulogic := '1';  -- reset signal
+        signal ce                 : std_ulogic := '0';  -- clock enable
 
-  --signal reset : std_logic := '1';
-  --signal clock : std_logic := '0';
-  --signal rst_n : std_logic;
-  --signal ce    : std_logic := '0';
-  
-  -- component generics
-  constant g_data_width   : natural := 32;
-  constant g_addr_width   : natural := 4;
-  constant g_tgd_width    : natural := 4;
-  constant g_buffer_depth : natural := 4;
+        signal valid_in     : std_ulogic;  -- signals valid input data
+        signal valid_out    : std_ulogic;  -- signals new valid output
+        signal cordic_busy  : std_ulogic;  -- signals cordic not ready for new inputs
+        signal cordic_ready : std_ulogic;  -- negated cordic_busy
+        signal end_of_file  : std_ulogic;
 
-  -- component ports
-  signal clk_i    : std_logic;
-  signal rst_i    : std_logic;
-  signal snk_i    : t_wbs_sink_in;
-  signal snk_o    : t_wbs_sink_out;
-  signal addr_o   : std_logic_vector(g_addr_width-1 downto 0);
-  signal data_o   : std_logic_vector(g_data_width-1 downto 0);
-  signal tgd_o    : std_logic_vector(g_tgd_width-1 downto 0);
-  signal dvalid_o : std_logic;
-  signal busy_i   : std_logic;
+        -- component generics
+        constant g_data_width   : natural := 32;
+        constant g_addr_width   : natural := 4;
+        constant g_tgd_width    : natural := 4;
+        constant g_buffer_depth : natural := 4;
 
-  -- clock
-  signal Clk : std_logic := '1';
+        -- component ports
+        signal clk_i    : std_logic;
+        signal rst_i    : std_logic;
+        signal snk_i    : t_wbs_sink_in;
+        signal snk_o    : t_wbs_sink_out;
+        signal addr_o   : std_logic_vector(g_addr_width-1 downto 0);
+        signal data_o   : std_logic_vector(g_data_width-1 downto 0);
+        signal tgd_o    : std_logic_vector(g_tgd_width-1 downto 0);
+        signal dvalid_o : std_logic;
+        signal busy_i   : std_logic;
 
-begin  -- architecture test
+    begin  -- architecture test
 
-  -- component instantiation
-  DUT: entity work.wb_stream_sink
-    generic map (
-      g_data_width   => g_data_width,
-      g_addr_width   => g_addr_width,
-      g_tgd_width    => g_tgd_width,
-      g_buffer_depth => g_buffer_depth)
-    port map (
-      clk_i    => clk_i,
-      rst_i    => rst_i,
-      snk_i    => snk_i,
-      snk_o    => snk_o,
-      addr_o   => addr_o,
-      data_o   => data_o,
-      tgd_o    => tgd_o,
-      dvalid_o => dvalid_o,
-      busy_i   => busy_i);
+        p_clk_gen (
+            clk      => clk,
+              c_FREQ => c_CLK_FREQ);
 
-  -- clock generation
-  Clk <= not Clk after 10 ns;
+          p_rst_gen (
+              clk        => clk,
+                rst      => rst,
+                c_CYCLES => 2);
 
-  -- waveform generation
-  WaveGen_Proc: process
-  begin
-    -- insert signal assignments here
+          p_ce_gen (
+              clk        => clk,
+                ce       => ce,
+                rst      => rst,
+                c_CYCLES => c_CYCLES_TO_CE);
 
-    tset
 
-    wait until Clk = '1';
-  end process WaveGen_Proc;
+          p_read_tsv_file_signed (
+              c_INPUT_FILE_NAME    => c_INPUT_FILE,
+                c_SAMPLES_PER_LINE => 3,              -- number of inputs
+                c_OUTPUT_WIDTH     => c_INPUT_WIDTH,  --input for the testbench, output for
+                                                      --the procedure
+                clk                => clk,
+                rst                => rst,
+                ce                 => clk,
+                req                => not_busy,
+                sample(0)          => addr,
+                sample(1)          => data,
+                sample(2)          => tgd,
+                valid              => valid_in,
+                end_of_file        => end_of_file);
 
-  
+        snk_i. <=
+          
 
-end architecture test;
+          -- component instantiation
+          UUT : entity work.wb_stream_sink
+              generic map (
+                  g_data_width     => g_data_width,
+                    g_addr_width   => g_addr_width,
+                    g_tgd_width    => g_tgd_width,
+                    g_buffer_depth => g_buffer_depth)
+              port map (
+                  clk_i      => clk_i,
+                    rst_i    => rst_i,
+                    snk_i    => snk_i,
+                    snk_o    => snk_o,
+                    addr_o   => addr_o,
+                    data_o   => data_o,
+                    tgd_o    => tgd_o,
+                    dvalid_o => dvalid_o,
+                    busy_i   => busy_i);
+
+
+          -- waveform generation
+          WaveGen_Proc : process
+            begin
+                -- insert signal assignments here
+
+                tset
+
+                    wait until Clk = '1';
+              end process WaveGen_Proc;
+
+
+
+              end architecture test;
 
 -------------------------------------------------------------------------------
 
-configuration wb_stream_sink_tb_test_cfg of wb_stream_sink_tb is
-  for test
-  end for;
-end wb_stream_sink_tb_test_cfg;
+                  configuration wb_stream_sink_tb_test_cfg of wb_stream_sink_tb is
+                    for test
+                  end for;
+                  end wb_stream_sink_tb_test_cfg;
 
 -------------------------------------------------------------------------------
 
-end entity wb_stream_sink_tb;
+                  end entity wb_stream_sink_tb;
 
 -------------------------------------------------------------------------------
