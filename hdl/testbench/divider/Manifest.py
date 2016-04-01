33e45fd35c1903d5692f248ed63a5d18d5cd3bf6
Aylons Hazzud <gustavo@aylons.com.br>
2016-04-01 18:01:09 -0300

    Updated Manifest.py to new version
    Manifest.py for simulation has new variables. Also, it a must explicity
    include general cores due to changes in structure of the project.

--- hdl/testbench/divider/Manifest.py
+++ hdl/testbench/divider/Manifest.py
@@ -1,8 +1,15 @@
 action = "simulation"
-target = "xilinx"
-syn_device = "xc6vlx240t"
 sim_tool = "modelsim"
 top_module = "div_tb"
+vcom_opt = "-2008"
 
-modules = {"local" : ["../../", "../../sim/test_pkg/"]}
+target = "xilinx"
+syn_device = "xc7a200t"
+
+
+machine_pkg = "uvx_130M" # uvx_130M sirius_130M
+
+
+modules = {"local" : ["../../", 
+                        "../../ip_cores/general-cores/", "../../sim/test_pkg/"]}
 files = ["div_tb.vhd"]
