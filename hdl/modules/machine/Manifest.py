8f22729afe8e0d19d50b3bdf86c3426324072679
Aylons Hazzud <gustavo@aylons.com.br>
2016-04-06 17:16:28 -0300

    Added version for Sirius using FMC250

--- hdl/modules/machine/Manifest.py
+++ hdl/modules/machine/Manifest.py
@@ -3,6 +3,9 @@
 if (machine_pkg == "uvx_130M"):
     modules = { "local" : "uvx_130M"}
 elif (machine_pkg == "sirius_130M"):
-    modules = { "local" : "sirius_130M"}
+    modules = { "local" : "sirius_130M"} 
+elif (machine_pkg == "sirius_250M"):
+    modules = { "local" : "sirius_250M"}
+   
 else:
     quit("Invalid machine module! Check machine_pkg variable!")
