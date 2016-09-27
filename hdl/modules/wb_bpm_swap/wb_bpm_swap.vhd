129fb300dcc8df65eb47a409d5c485ecacf505ba
Lucas Russo <lerwys@gmail.com>
2016-09-27 10:27:03 -0300

    modules/wb_bpm_swap/*: fix wishbone address width for register bank

--- hdl/modules/wb_bpm_swap/wb_bpm_swap.vhd
+++ hdl/modules/wb_bpm_swap/wb_bpm_swap.vhd
@@ -78,7 +78,7 @@ end wb_bpm_swap;
 
 architecture rtl of wb_bpm_swap is
 
-  constant c_periph_addr_size               : natural := 3+2;
+  constant c_periph_addr_size               : natural := 1+2;
 
   signal fs_rst_n                           : std_logic;
 
@@ -102,7 +102,7 @@ architecture rtl of wb_bpm_swap is
   port (
     rst_n_i               : in     std_logic;
     clk_sys_i             : in     std_logic;
-    wb_adr_i              : in     std_logic_vector(2 downto 0);
+    wb_adr_i              : in     std_logic_vector(0 downto 0);
     wb_dat_i              : in     std_logic_vector(31 downto 0);
     wb_dat_o              : out    std_logic_vector(31 downto 0);
     wb_cyc_i              : in     std_logic;
@@ -159,7 +159,7 @@ begin
   port map (
     rst_n_i                                 => rst_n_i,
     clk_sys_i                               => clk_sys_i,
-    wb_adr_i                                => wb_slv_adp_out.adr(2 downto 0),
+    wb_adr_i                                => wb_slv_adp_out.adr(0 downto 0),
     wb_dat_i                                => wb_slv_adp_out.dat,
     wb_dat_o                                => wb_slv_adp_in.dat,
     wb_cyc_i                                => wb_slv_adp_out.cyc,
