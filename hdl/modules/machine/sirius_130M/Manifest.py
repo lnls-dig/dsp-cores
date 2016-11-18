f0bd96079206a169a7758cf26d50a816687bc92b
Lucas Russo <lerwys@gmail.com>
2016-11-18 12:00:25 -0200

    modules/machine: only include .xci and .coe files
    All of the other files will be generated on
    demand by Vivado. Also, including the .vhd
    files will give an error, as it won't find
    blk_mem_gen module (we don't compile it).

--- hdl/modules/machine/sirius_130M/Manifest.py
+++ hdl/modules/machine/sirius_130M/Manifest.py
@@ -1,15 +1,10 @@
 files = [  "machine_pkg.vhd",
            "dds_cos_lut.vhd",
-           "cos_lut_sirius_52_203/cos_lut_sirius_52_203.mif",
-           "cos_lut_sirius_52_203/cos_lut_sirius_52_203_ooc.xdc",
+           "cos_lut_sirius_52_203/cos_lut_sirius_52_203.coe",
            "cos_lut_sirius_52_203/cos_lut_sirius_52_203.xci",
-           "cos_lut_sirius_52_203/cos_lut_sirius_52_203.vhd",
            "dds_sin_lut.vhd",
-           "sin_lut_sirius_52_203/sin_lut_sirius_52_203.mif",
-           "sin_lut_sirius_52_203/sin_lut_sirius_52_203_ooc.xdc",
+           "sin_lut_sirius_52_203/sin_lut_sirius_52_203.coe",
            "sin_lut_sirius_52_203/sin_lut_sirius_52_203.xci",
-           "sin_lut_sirius_52_203/sin_lut_sirius_52_203.vhd",
-           "sw_windowing_n_251_tukey_0_2/sw_windowing_n_251_tukey_0_2.mif",
-           "sw_windowing_n_251_tukey_0_2/sw_windowing_n_251_tukey_0_2_ooc.xdc",
-           "sw_windowing_n_251_tukey_0_2/sw_windowing_n_251_tukey_0_2.xci",
-           "sw_windowing_n_251_tukey_0_2/sw_windowing_n_251_tukey_0_2.vhd"]
+           "sw_windowing_n_251_tukey_0_2/sw_windowing_n_251_tukey_0_2.coe",
+           "sw_windowing_n_251_tukey_0_2/sw_windowing_n_251_tukey_0_2.xci"
+           ]
