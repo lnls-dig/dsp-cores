a392a3e718a293586a9a7a3e1dde6cf0ee3ceb28
Lucas Russo <lerwys@gmail.com>
2017-06-19 14:15:47 -0300

    modules/machine: rename machine modules to better reflect its meaning

--- hdl/modules/machine/Manifest.py
+++ hdl/modules/machine/Manifest.py
@@ -1,13 +1,13 @@
 # TODO: if hdlmake allowed for importing python packages in inner modules, this could existing directories directly
 
-if (machine_pkg == "uvx_130M"):
-    modules = { "local" : "uvx_130M"}
-elif (machine_pkg == "sirius_130M"):
-    modules = { "local" : "sirius_130M"}
-elif (machine_pkg == "sirius_250M"):
-    modules = { "local" : "sirius_250M"}
-elif (machine_pkg == "uvx_250M"):
-    modules = { "local" : "uvx_250M"}
+if (machine_pkg == "uvx_sr_130M"):
+    modules = { "local" : "uvx_sr_130M"}
+elif (machine_pkg == "uvx_sr_250M"):
+    modules = { "local" : "uvx_sr_250M"}
+elif (machine_pkg == "sirius_sr_130M"):
+    modules = { "local" : "sirius_sr_130M"}
+elif (machine_pkg == "sirius_sr_250M"):
+    modules = { "local" : "sirius_sr_250M"}
 elif (machine_pkg == "pbpm_fmcpico1M"):
     modules = { "local" : "pbpm_fmcpico1M"}
 else:
