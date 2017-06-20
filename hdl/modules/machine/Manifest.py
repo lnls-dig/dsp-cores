a970e8937c5774fb3960680c6c158f0ef0d4f6f4
Lucas Russo <lerwys@gmail.com>
2017-06-20 14:14:45 -0300

    hdl/*/machine: add new Sirius Booster DDS tables
    This uses the following numerology:
    
    - Sirius Booster
    
        DDS LUT:
        sin(2*pi*52/181*i);   i = 0, 1, 2, 3, ..., 180
        cos(2*pi*52/181*i);   i = 0, 1, 2, 3, ..., 180
    
        TBT decimation rate = 362
        FOFB decimation rate = 1810
        MONIT decimation rate = FOFB decimation rate x 12000

--- hdl/modules/machine/Manifest.py
+++ hdl/modules/machine/Manifest.py
@@ -8,6 +8,8 @@ elif (machine_pkg == "sirius_sr_130M"):
     modules = { "local" : "sirius_sr_130M"}
 elif (machine_pkg == "sirius_sr_250M"):
     modules = { "local" : "sirius_sr_250M"}
+elif (machine_pkg == "sirius_bo_250M"):
+    modules = { "local" : "sirius_bo_250M"}
 elif (machine_pkg == "pbpm_fmcpico1M"):
     modules = { "local" : "pbpm_fmcpico1M"}
 else:
