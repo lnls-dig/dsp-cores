6f8f2dad06adcc123aead479f18b9c90fa7a59f2
Lucas Russo <lerwys@gmail.com>
2017-08-07 08:47:16 -0300

    modules/position_calc: fix mixer output valid bit instead of hardcoded '1'

--- hdl/modules/position_calc/position_calc.vhd
+++ hdl/modules/position_calc/position_calc.vhd
@@ -645,7 +645,7 @@ begin
   mix_ch2_q_o <= std_logic_vector(resize(signed(full_q(2)), g_IQ_width));
   mix_ch3_i_o <= std_logic_vector(resize(signed(full_i(3)), g_IQ_width));
   mix_ch3_q_o <= std_logic_vector(resize(signed(full_q(3)), g_IQ_width));
-  mix_valid_o <= '1';
+  mix_valid_o <= iq_valid(0);
   mix_ce_o    <= ce_adc(0);
 
   tbt_decim_ch0_i_o <= tbt_i(0);
