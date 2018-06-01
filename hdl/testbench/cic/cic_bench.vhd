e785176aa37e050436c84ce39f2d5ab06cb887fa
Lucas Russo <lerwys@gmail.com>
2018-06-01 12:29:38 -0300

    testbench/cic: update testbench with CIC synchronization

--- hdl/testbench/cic/cic_bench.vhd
+++ hdl/testbench/cic/cic_bench.vhd
@@ -41,7 +41,7 @@ architecture str of cic_bench is
   -----------------------------------------------------------------------------
   -- Internal signal declarations
   -----------------------------------------------------------------------------
-  constant c_input_freq      : real    := 120.0e6;
+  constant c_input_freq      : real    := 100.0e6;
   constant c_clock_period    : time    := 1.0 sec /(2.0*c_input_freq);
   constant c_cycles_to_reset : natural := 4;
 
@@ -49,12 +49,15 @@ architecture str of cic_bench is
   signal reset   : std_logic := '1';
   signal ce      : std_logic := '1';
   signal ce_out  : std_logic := '0';
+  signal sw_out  : std_logic := '0';
+  signal data_tag : std_logic_vector(0 downto 0) := "0";
+  signal data_tag_en : std_logic := '0';
 
   constant c_input_width     : natural := 24;
   constant c_output_width    : natural := 26;
   constant c_diff_delay      : natural := 1;
   constant c_stages      : natural := 2;
-  constant c_decimation_rate : natural := 382;
+  constant c_decimation_rate : natural := 10;
   constant c_bus_width       : natural := natural(ceil(log2(real(c_decimation_rate))))+2;
 
   signal data_in   : std_logic_vector(c_input_width-1 downto 0) := (others => '0');
@@ -64,24 +67,29 @@ architecture str of cic_bench is
 
   component cic_dyn is
     generic (
-      g_input_width  : natural;
-      g_output_width : natural;
-      g_stages       : natural;
-      g_delay        : natural;
-      g_max_rate     : natural;
-      g_bus_width    : natural;
-      g_with_ce_synch    : boolean;
-      g_round_convergent : natural);
+      g_input_width      : natural := 16;
+      g_output_width     : natural := 16;
+      g_stages           : natural := 1;      -- aka "N"
+      g_delay            : natural := 1;      -- aka "M"
+      g_max_rate         : natural := 2048;   -- Max decimation rate
+      g_bus_width        : natural := 11;     -- Decimation ratio bus width.
+      g_with_ce_synch    : boolean := false;
+      g_tag_width        : natural := 1;      -- Input data tag width
+      g_round_convergent : natural := 0
+      );
     port (
-      clock_i  : in  std_logic                                   := '0';
-      reset_i  : in  std_logic                                   := '0';
-      ce_i     : in  std_logic                                   := '0';
-      ce_out_i : in  std_logic                                   := '0';
-      valid_i  : in  std_logic                                   := '1';
-      data_i   : in  std_logic_vector(g_input_width-1 downto 0)  := (others => '0');
-      ratio_i  : in  std_logic_vector(g_bus_width-1 downto 0)    := (others => '0');
-      data_o   : out std_logic_vector(g_output_width-1 downto 0) := (others => '0');
-      valid_o  : out std_logic                                   := '0');
+      clock_i     : in  std_logic                                      := '0';
+      reset_i     : in  std_logic                                      := '0';
+      ce_i        : in  std_logic                                      := '0';
+      ce_out_i    : in  std_logic                                      := '0';
+      valid_i     : in  std_logic                                      := '1';
+      data_i      : in  std_logic_vector(g_input_width-1 downto 0)     := (others => '0');
+      data_tag_i  : in  std_logic_vector(g_tag_width-1 downto 0) := (others => '0');
+      data_tag_en_i : in std_logic                                     := '0';
+      ratio_i     : in  std_logic_vector(g_bus_width-1 downto 0)       := (others => '0');
+      data_o      : out std_logic_vector(g_output_width-1 downto 0)    := (others => '0');
+      valid_o     : out std_logic                                      := '0'
+      );
   end component cic_dyn;
 
 begin  -- architecture str
@@ -89,23 +97,48 @@ begin  -- architecture str
 
   clk_gen : process
   begin
-    clock <= '0';
-    wait for c_clock_period;
     clock <= '1';
-    wait for c_clock_period;
+    wait for c_clock_period/2;
+    clock <= '0';
+    wait for c_clock_period/2;
   end process;
 
   ce_gen : process
   begin
     if reset = '0' then
       ce_out <= '0';
-      wait for 382*c_clock_period;
+      wait for 9*c_clock_period;
       ce_out <= '1';
       wait for c_clock_period;
     else
       ce_out <= '0';
       wait for c_clock_period;
     end if;
+  end process;
+
+  sw_gen : process
+  begin
+    if reset = '0' then
+      sw_out <= '0';
+      wait for 10*c_clock_period;
+      sw_out <= '1';
+      wait for 10*c_clock_period;
+    else
+      sw_out <= '0';
+      wait for c_clock_period;
+      sw_out <= '1';
+      wait for 5*c_clock_period;
+    end if;
+  end process;
+
+  data_tag(0) <= sw_out;
+
+  data_tag_en_gen : process
+  begin
+    data_tag_en <= '0';
+    wait for 1000*c_clock_period;
+    data_tag_en <= '1';
+    wait;
   end process;
 
   rst_gen : process(clock)
@@ -154,6 +187,8 @@ begin  -- architecture str
       ce_i      => ce,
       ce_out_i  => ce_out,
       data_i    => data_in,
+      data_tag_i => data_tag,
+      data_tag_en_i => data_tag_en,
       ratio_i   => std_logic_vector(to_unsigned(c_decimation_rate, c_bus_width)),
       data_o    => data_out,
       valid_o   => cic_valid);
