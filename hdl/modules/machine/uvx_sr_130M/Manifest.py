98b18de2f0d60acc7e4fb0c97b6c569a78a62300
Lucas Russo <lerwys@gmail.com>
2019-03-21 10:33:31 -0300

    machine/uvx_sr_130M: change workflow to vhd/verilog files instead of XCI
    XCI files were giving too much trouble in our workflow.
    Every Vivado version forced to change IPs and to recompile
    everything. Plus, the "preferred language" issue
    (https://www.xilinx.com/support/answers/51041.html)

--- hdl/modules/machine/uvx_sr_130M/Manifest.py
+++ hdl/modules/machine/uvx_sr_130M/Manifest.py
@@ -1,12 +1,15 @@
 files = [  "machine_pkg.vhd",
            "dds_cos_lut.vhd",
            "cos_lut_uvx_35_148/cos_lut_uvx_35_148.coe",
-           "cos_lut_uvx_35_148/cos_lut_uvx_35_148.xci",
+           "cos_lut_uvx_35_148/cos_lut_uvx_35_148.mif",
+           "cos_lut_uvx_35_148/synth/cos_lut_uvx_35_148.vhd",
            "dds_sin_lut.vhd",
            "sin_lut_uvx_35_148/sin_lut_uvx_35_148.coe",
-           "sin_lut_uvx_35_148/sin_lut_uvx_35_148.xci",
+           "sin_lut_uvx_35_148/sin_lut_uvx_35_148.mif",
+           "sin_lut_uvx_35_148/synth/sin_lut_uvx_35_148.vhd",
            "sw_windowing_n_251_tukey_0_2/sw_windowing_n_251_tukey_0_2.coe",
-           "sw_windowing_n_251_tukey_0_2/sw_windowing_n_251_tukey_0_2.xci"
+           "sw_windowing_n_251_tukey_0_2/sw_windowing_n_251_tukey_0_2.mif",
+           "sw_windowing_n_251_tukey_0_2/synth/sw_windowing_n_251_tukey_0_2.vhd"
            ]
 
 
