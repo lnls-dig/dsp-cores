6e43856d31494ba75b8386366ed24fad5377473b
Lucas Russo <lerwys@gmail.com>
2019-04-02 08:16:13 -0300

    modules/wb_position_calc: add trigger2tag to Manifest

--- hdl/modules/wb_position_calc/Manifest.py
+++ hdl/modules/wb_position_calc/Manifest.py
@@ -4,6 +4,7 @@ files = [  "xwb_position_calc_core.vhd",
            "position_calc_cdc_fifo.vhd",	
            "position_calc_counters_single.vhd",
            "position_calc_counters.vhd",
+           "trigger2tag.vhd",
            "wbgen/wb_pos_calc_regs_pkg.vhd",
            "wbgen/wb_pos_calc_regs.vhd"
         ]
