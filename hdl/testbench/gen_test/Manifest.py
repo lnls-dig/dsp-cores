e7ffcfb8f384fe34fb700128714b03f234655675
Lucas Russo <lerwys@gmail.com>
2020-06-02 16:51:16 -0300

    testbench: remove outdated/broken gen_test testbench

--- hdl/testbench/gen_test/Manifest.py
+++
@@ -1,9 +0,0 @@
-action = "simulation"
-target = "xilinx"
-syn_device = "xc6vlx240t"
-sim_tool = "modelsim"
-top_module = "gen_test"
-
-modules = {"local" : ["../../", "../../top/ml605/bpm_dsp_example/","../../sim/test_pkg/"]}
-
-files = ["gen_test.vhd"]
