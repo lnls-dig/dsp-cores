cadb6dc208f57920a6dc50c616e49e0a1afda5ce
Lucas Russo <lerwys@gmail.com>
2020-06-02 17:08:38 -0300

    modules/dsp_cores_pkg.vhd: move downconv component to lnls-dig/bpm-gw repo

--- hdl/modules/dsp_cores_pkg.vhd
+++ hdl/modules/dsp_cores_pkg.vhd
@@ -311,29 +311,6 @@ package dsp_cores_pkg is
       valid_o : out std_logic);
   end component fixed_dds;
 
-  component downconv is
-    generic (
-      g_input_width      : natural := 16;
-      g_mixed_width      : natural := 24;
-      g_output_width     : natural := 32;
-      g_phase_width      : natural := 8;
-      g_sin_file         : string  := "./dds_sin.nif";
-      g_cos_file         : string  := "./dds_cos.nif";
-      g_number_of_points : natural := 6;
-      g_diff_delay       : natural := 2;
-      g_stages           : natural := 3;
-      g_decimation_rate  : natural := 1000);
-    port (
-      signal_i : in  std_logic_vector(g_input_width-1 downto 0);
-      clk_i    : in  std_logic;
-      ce_i     : in  std_logic;
-      rst_i    : in  std_logic;
-      phase_i  : in  std_logic_vector(g_phase_width-1 downto 0);
-      I_o      : out std_logic_vector(g_output_width-1 downto 0);
-      Q_o      : out std_logic_vector(g_output_width-1 downto 0);
-      valid_o  : out std_logic);
-  end component downconv;
-
   component div_fixedpoint is
     generic (
       G_DATAIN_WIDTH : integer range 2 to 48;
