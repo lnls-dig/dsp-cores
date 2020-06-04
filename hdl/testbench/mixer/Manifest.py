e8f6eafb23ac7708f6bd6baacfe23e1627519065
Lucas Russo <lerwys@gmail.com>
2020-06-04 09:35:45 -0300

    {modules,testbench}: move fixed_dds module to lnls-dig/bpm-gw repo

--- hdl/testbench/mixer/Manifest.py
+++
@@ -1,9 +0,0 @@
-action = "simulation"
-target = "xilinx"
-syn_device = "xc7a200t"
-sim_tool = "modelsim"
-top_module = "mixer_bench"
-
-modules = {"local" : ["../../", "../../ip_cores/general-cores/","../../sim/test_pkg/"]}
-
-files = ["mixer_bench.vhd", "blk_mem_gen_v8_2.vhd"]
--- hdl/testbench/mixer/wave.do
+++
@@ -1,5 +0,0 @@
-onerror {resume}
-quietly WaveActivateNextPane {} 0
-add wave -noupdate /mixer_bench/adc_data
-add wave -noupdate /mixer_bench/endoffile
-log //uut/*
