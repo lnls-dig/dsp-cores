f584baad09b48d21d66e037297d5f64a19aa1aff
Lucas Russo <lerwys@gmail.com>
2020-07-09 12:01:17 -0300

    modules/cic: fix tag detection to work when valid/ce are not always 1
    When valid/ce are not always one we won't be able
    to sync decimation_strobe with data_tag, as when valid
    becomes '1' the decimation strobe has already happened.

--- hdl/modules/cic/cic_dyn.vhd
+++ hdl/modules/cic/cic_dyn.vhd
@@ -178,7 +178,7 @@ begin  -- architecture str
     end if;
   end process;
 
-  data_tag_change <= '1' when valid_i = '1' and data_tag_i /= data_tag_d0 else '0';
+  data_tag_change <= '1' when data_tag_i /= data_tag_d0 else '0';
   data_tag_change_d0 <= '1' when data_tag_d0 /= data_tag_d1 else '0';
 
   p_sync_cic_fsm : process(clk_i)
@@ -193,13 +193,10 @@ begin  -- architecture str
       else
         if ce_i = '1' then
 
-          -- tags are only valid if valid_i = '1'
-          if valid_i = '1' then
-            data_tag_d0 <= data_tag_i;
-            -- To check for a transition we need another clock cycle. So, use a
-            -- delayed version of data_tag
-            data_tag_d1 <= data_tag_d0;
-          end if;
+          data_tag_d0 <= data_tag_i;
+          -- To check for a transition we need another clock cycle. So, use a
+          -- delayed version of data_tag
+          data_tag_d1 <= data_tag_d0;
 
           -- Desync clear
           if data_tag_desync_cnt_rst_i = '1' then
