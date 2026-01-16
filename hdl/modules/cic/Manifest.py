1801627121ba50c6bb7bca3cafa506c7d5b9b616
Guilherme Ricioli <guilherme.riciolic@gmail.com>
2026-01-16 16:30:30 -0300

    modules/cic/Manifest.py: default to VHDL implementation

--- hdl/modules/cic/Manifest.py
+++ hdl/modules/cic/Manifest.py
@@ -1,5 +1,10 @@
 files = [  "cic_dyn.vhd", "log2.v", "decimation_strober.vhd", "cic_dual.vhd"  ]
 
+try:
+    use_cic_decim_vhdl
+except NameError:
+    use_cic_decim_vhdl = True
+
 if use_cic_decim_vhdl:
     files.append("cic_decim.vhd")
 else:
